4fb17219d98c53f83fea438f7e0207b5359d8d26
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2020-03-25 14:13:12 -0400

    -add cartIcon and cartDropdown + reducers

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon-component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo'></Logo>
@@ -18,16 +20,19 @@ const Header = ({ currentUser }) => (
       <Link className='option' to='/shop'>CONTACT</Link>
       {
         currentUser ?
-          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+          (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
           :
-          <Link className='option' to='/signin'>SIGN IN</Link>
+          (<Link className='option' to='/signin'>SIGN IN</Link>)
       }
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropDown />}
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 })
 
 // connect is a HOC that 'juices up' a component --in this case the Header component-- with props that have been mapped from the root reducer according to mapStateToProps 
