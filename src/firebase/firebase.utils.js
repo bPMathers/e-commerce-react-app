a431366008e7d05f064bd20dedf8552c541bc613
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2020-03-29 16:09:31 -0400

    upload for teacher assistance regarding firestore collectionRef problem

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
   measurementId: "G-MPVFDPBFD9"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
@@ -36,9 +38,13 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
-}
+};
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+};
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
