501da4030df185efeb4c9a606a6c86f54deba1b1
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2020-04-04 15:46:41 -0400

    -implement payment route for stripe with express node backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_x4QSIOj6sHzuCIHL5zWN0L1q00XEoG7xtM';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful')
-  }
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment. Please make sure you use the provided credit card');
+    });
+  };
 
   return (
     <StripeCheckout
