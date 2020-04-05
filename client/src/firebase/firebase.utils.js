8e490efe4454eb2a3241e233b3760a42749ef0da
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2020-04-05 00:19:23 -0400

    -add cart persistence to firebase

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyBBM-ir-vbFMlzR23_Mp3lEETt51awSd6A",
-  authDomain: "e-commerce-portfolio-app.firebaseapp.com",
-  databaseURL: "https://e-commerce-portfolio-app.firebaseio.com",
-  projectId: "e-commerce-portfolio-app",
-  storageBucket: "e-commerce-portfolio-app.appspot.com",
-  messagingSenderId: "493292336149",
-  appId: "1:493292336149:web:a5d21477024759150d0a1c",
-  measurementId: "G-MPVFDPBFD9"
+  apiKey: 'AIzaSyBBM-ir-vbFMlzR23_Mp3lEETt51awSd6A',
+  authDomain: 'e-commerce-portfolio-app.firebaseapp.com',
+  databaseURL: 'https://e-commerce-portfolio-app.firebaseio.com',
+  projectId: 'e-commerce-portfolio-app',
+  storageBucket: 'e-commerce-portfolio-app.appspot.com',
+  messagingSenderId: '493292336149',
+  appId: '1:493292336149:web:a5d21477024759150d0a1c',
+  measurementId: 'G-MPVFDPBFD9',
 };
 
 firebase.initializeApp(config);
@@ -30,8 +30,8 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
-      })
+        ...additionalData,
+      });
     } catch (error) {
       console.log('error creating user', error.message);
     }
@@ -39,46 +39,61 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+export const getUserCartRef = async (userId) => {
+  const cartsRef = firestore.collection('carts').where('userId', '==', userId);
+  const snapShot = await cartsRef.get();
+
+  if (snapShot.empty) {
+    const cartDocRef = firestore.collection('carts').doc();
+    await cartDocRef.set({ userId, cartItems: [] });
+    return cartDocRef;
+  } else {
+    return snapShot.docs[0].ref;
+  }
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
   const collectionRef = firestore.collection(collectionKey);
 
   const batch = firestore.batch();
-  objectsToAdd.forEach(obj => {
+  objectsToAdd.forEach((obj) => {
     const newDocRef = collectionRef.doc();
     batch.set(newDocRef, obj);
   });
 
-  return await batch.commit()
-
+  return await batch.commit();
 };
 
-export const convertCollectionsSnapshotToMap = collections => {
-  const transformedCollection = collections.docs.map(doc => {
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
     const { title, items } = doc.data();
 
     return {
       routeName: encodeURI(title.toLowerCase()),
       id: doc.id,
       title,
-      items
-    }
+      items,
+    };
   });
 
   return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
   }, {});
-}
+};
 
 export const getCurrentUser = () => {
   // transform observer subscription into promise to be consumed by saga generator function
   return new Promise((resolve, reject) => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       unsubscribe();
       resolve(userAuth);
-    }, reject)
-  })
-}
+    }, reject);
+  });
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
